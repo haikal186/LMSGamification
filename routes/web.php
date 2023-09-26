f0f03737898757d0261266cf9916a617b569ecd0
haikal186 <haikalhadi186@gmail.com>
2023-09-26 11:40:23 +0800

    route updated

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,6 +26,8 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::prefix('profile')->controller(ProfileController::class)->group(function () {
     Route::get('/', 'index')->name('profile.index');
+    Route::get('/create', 'create')->name('profile.create');
+    Route::post('/store', 'store')->name('profile.store');
 });
 
 
