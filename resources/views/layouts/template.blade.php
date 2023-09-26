d4fd7ac62ee8a2a98237edb9cd4badbce2e9ceba
haikal186 <haikalhadi186@gmail.com>
2023-09-26 11:39:23 +0800

    fix include to yield in body

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -610,7 +610,7 @@
         <div class="content-body">
             <!-- row -->
 			<div class="container-fluid">
-                @include('layouts.content')
+                @yield('content')
             </div>
         </div>
         <!--**********************************
@@ -648,13 +648,13 @@
     ***********************************-->
     <!-- Required vendors -->
     <script src="{!! asset('/vendor/global/global.min.js') !!}"></script>
-	{{-- <script src="{!! asset('/vendor/chart.js/Chart.bundle.min.js') !!}"></script> --}}
+	<script src="{!! asset('/vendor/chart.js/Chart.bundle.min.js') !!}"></script>
 	<script src="{!! asset('/vendor/jquery-nice-select/js/jquery.nice-select.min.js') !!}"></script>
 	
 	<!-- Apex Chart -->
 	<script src="{!! asset('/vendor/apexchart/apexchart.js') !!}"></script>
 	
-	{{-- <script src="{!! asset('/vendor/chart.js/Chart.bundle.min.js') !!}"></script> --}}
+	<script src="{!! asset('/vendor/chart.js/Chart.bundle.min.js') !!}"></script>
 	
 	<!-- Chart piety plugin files -->
     <script src="{!! asset('/vendor/peity/jquery.peity.min.js') !!}"></script>
@@ -662,11 +662,13 @@
 	<!-- Dashboard 1 -->
 	<script src="{!! asset('/js/dashboard/dashboard-1.js') !!}"></script>
 	
-	{{-- <script src="{!! asset('/vendor/owl-carousel/owl.carousel.js') !!}"></script> --}}
+	<script src="{!! asset('/vendor/owl-carousel/owl.carousel.js') !!}"></script>
 	
-    <script src="{!! asset('/js/custom.min.js') !!}"></script>
+	<script src="{!! asset('/vendor/bootstrap-datepicker-master/js/bootstrap-datepicker.min.js') !!}"></script>
+	{{-- <script src="{!! asset('/vendor/datatables/js/jquery.dataTables.min.js') !!}"></script>
+	<script src="{!! asset('/js/plugins-init/datatables.init.js') !!}"></script> --}}
+	<script src="{!! asset('/js/custom.min.js') !!}"></script>
 	<script src="{!! asset('/js/dlabnav-init.js') !!}"></script>
-	
     
 	<script>
 		function JobickCarousel()
