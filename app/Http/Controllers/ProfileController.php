38e9e26bd5ef7c0c503ab0069a85ae19a0ef89e8
haikal186 <haikalhadi186@gmail.com>
2023-09-26 11:37:30 +0800

    add request profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -9,5 +10,21 @@ class ProfileController extends Controller
     public function index()
     {
         return view('profile.index');
+    }
+
+    public function create()
+    {
+        return view('profile.create');
+    }
+
+    public function store(Request $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+        return to_route('profile.index');
     }
 }
