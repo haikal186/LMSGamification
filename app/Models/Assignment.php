1dc8ea253fc515ac550dcf913538fac8558edb01
haikal186 <haikalhadi186@gmail.com>
2024-01-04 14:55:45 +0800

    create assignment model

---
+++ app/Models/Assignment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Course;
+use App\Models\Submission;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Assignment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'assigned_date',
+        'deadline',
+        'course_id',
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'course_id');
+    }
+
+    public function submissions()
+    {
+        return $this->hasMany(Assignment::class);
+
+    }
+}
