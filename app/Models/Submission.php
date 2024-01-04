341123795ba52076dcd2a3e65e1786f6632f0bce
haikal186 <haikalhadi186@gmail.com>
2024-01-04 14:55:57 +0800

    create submission model

---
+++ app/Models/Submission.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Submission extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'status',
+        'submission_date',
+        'assignment_id',
+        'user_id',
+    ];
+
+    public function assignment()
+    {
+        return $this->belongsTo(Assignment::class,'assignment_id');
+    }
+}
