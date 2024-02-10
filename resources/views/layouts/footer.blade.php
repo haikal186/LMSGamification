79efef84efd8eb4618c2797d2b4d73e17d92379c
haikal186 <haikalhadi186@gmail.com>
2024-02-10 15:01:33 +0800

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="copyright">
-    <p>Copyright © Designed &amp; Developed by <a href="https://dexignlab.com/" target="_blank">DexignLab</a> 2021</p>
+    <p>Copyright © Designed &amp; Developed by <a href="#" target="_blank">Muhammad Haikal Bin Abdul Hadi</a> 2024</p>
 </div>
