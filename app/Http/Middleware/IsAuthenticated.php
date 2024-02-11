a763824b362ce2901ead0135ff75a7e76cf31823
haikal186 <haikalhadi186@gmail.com>
2024-02-11 12:18:35 +0800

    add variable file to display image

--- app/Http/Middleware/IsAuthenticated.php
+++ app/Http/Middleware/IsAuthenticated.php
@@ -26,6 +26,12 @@ class IsAuthenticated
 
             $role = $user->hasRole->name;
 
+            $file = $user->files->first();
+
+            if ($file) {
+                view()->share('file', $file);
+            }
+
             view()->share('short_name', $short_name);
             view()->share('role', $role);
 
