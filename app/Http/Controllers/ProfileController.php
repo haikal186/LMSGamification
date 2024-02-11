11465dac72244334f94fd457536bdb7fa82bc640
haikal186 <haikalhadi186@gmail.com>
2024-02-11 11:43:28 +0800

    create new controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,68 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function index()
-    {
-        $users = User::all();
-
-        return view('profile.index', compact('users'));
-    }
-
-    public function create()
-    {
-        return view('profile.create');
-    }
-
-    public function store(Request $request)
-    {
-        User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => $request->password
-        ]);
-
-        return to_route('profile.index');
-    }
-
-    public function edit(Request $request, $user_id)
-    {
-        $user = User::findOrFail($user_id);
-
-        return view('profile.edit',compact('user'));
-    }
-
-    public function update(Request $request, $user_id)
-    {
-        $user = User::findorFail($user_id);
-        
-        $user->update([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => $request->password,
-        ]);
-
-        return view('profile.index');
-
-    }
-
-    public function destroy(Request $request, $user_id)
-    {
-        $user = User::findOrFail($user_id);
-        $user->delete();
-        
-        return redirect()->route('profile.index')->with('success', 'User deleted successfully');
-    }
-
-    public function show($user_id)
-    {
-        $user = User::findOrFail($user_id);
-
-        $role = $user->hasRole;
-        return view('profile.show', compact('user','role'));
-    }
+    //
 }
