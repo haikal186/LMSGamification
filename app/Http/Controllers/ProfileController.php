7d448c2979898aa69f56ad0b3d69531cb2ec68e5
haikal186 <haikalhadi186@gmail.com>
2024-02-11 11:58:45 +0800

    create update function

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,5 +22,41 @@ class ProfileController extends Controller
         return view('profile.edit',compact('user'));
     }
 
+    public function update(Request $request, $user_id)
+    {
+        $user = User::findorFail($user_id);
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'updated_at' => now()
+        ]);
+
+        if ($request->hasFile('file')) {
+
+            if ($user->files()->exists()) {
+                $existingFile = $user->files()->first();
+                Storage::delete('public/uploads/' . $existingFile->name);
+                $existingFile->delete();
+            }
     
+            $file = $request->file('file');
+            $originalName = $file->getClientOriginalName();
+            $fileName = pathinfo($originalName, PATHINFO_FILENAME);
+            $extension = $file->getClientOriginalExtension();
+            $storedFileName = $fileName . '_' . time() . '.' . $extension;
+    
+            $file->storeAs('public/uploads', $storedFileName);
+    
+            $user->files()->create([
+                'original_name' => $originalName,
+                'name' => $storedFileName,
+                'file_path' => 'http://127.0.0.1:8000/storage/uploads/' . $storedFileName,
+                'file_type' => $file->getClientMimeType(),
+            ]);
+        }
+ 
+        return view('profile.show',compact('user'));
+    }
 }
