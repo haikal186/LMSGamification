2997eda22808840a8860aae6d0fa4e0a14775b36
haikal186 <haikalhadi186@gmail.com>
2024-02-11 12:18:54 +0800

    display profile picture

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,11 +1,15 @@
 <div class="dropdown header-profile2 ">
     <a class="nav-link " href="javascript:void(0);"  role="button" data-bs-toggle="dropdown">
         <div class="header-info2 d-flex align-items-center">
-            <img src="{{ asset('images/profile/pic1.jpg') }}" alt="">
+            @if($file)
+                <img src="{{ asset($file->file_path) }}" width="20" alt=""/>
+            @else
+                <img src="{{ asset('images/profile/pic1.jpg') }}" alt="">
+            @endif
             <div class="d-flex align-items-center sidebar-info">
                 <div>
                     <span class="font-w400 d-block">{{ $short_name }}</span>
-                    <small class="text-end font-w400">{{ $role }}</small>
+                    <small class="text-end font-w400">{{ ucfirst($role) }}</small>
                 </div>
                 <i class="fas fa-chevron-down"></i>
             </div>
@@ -25,7 +29,7 @@
 </div>
 <ul class="metismenu" id="menu">
     <li>
-        <a href="{{ route('home') }}" class="" href="javascript:void()" aria-expanded="false">
+        <a href="{{ route('home') }}" class="" aria-expanded="false">
             <i class="flaticon-025-dashboard"></i>
             <span class="nav-text">Dashboard</span>
         </a>
